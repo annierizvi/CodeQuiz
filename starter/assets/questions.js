663b008ceb07e585e29c57fa11577d17487188bf
Annie <anam.k@live.com>
2024-01-12 19:48:54 +0000

    added css questions

--- starter/assets/questions.js
+++ starter/assets/questions.js
@@ -8,18 +8,18 @@ const questions = [
       'Hyperlinks and Text Markup Language',
       'Home Tool Markup Language',
     ],
-    correct: 1,
+    answer: 'Hyper Text Markup Language',
   },
   {
     question:
       'Which of the following HTML elements is used for creating an unordered list?',
     answers: ['<ui>', '<i>', '<ul>', '<list>'],
-    correct: 2,
+    answer: '<ul>',
   },
   {
     question: 'Which HTML element is used to define a paragraph?',
     answers: ['<pg>', '<paragraph>', '<p>', '<text>'],
-    correct: 2,
+    correct: 'paragraph',
   },
   {
     question: 'What is the purpose of the <div> tag in HTML?',
@@ -29,6 +29,44 @@ const questions = [
       'For adding a line break',
       'For emphasizing text',
     ],
-    correct: 1,
+    correct: 'For creating a devision or a section',
   },
+  // CSS Questions
+  {
+    question: 'What does CSS stand for?',
+    choices: [
+      'Creative Style Sheets',
+      'Computer Style Sheets',
+      'Cascading Style Sheets',
+      'Colorful Style Sheets',
+    ],
+    answer: 'Cascading Style Sheets',
+  },
+  {
+    question:
+      'Which property is used to change the background color of an element?',
+    choices: ['bgcolor', 'color', 'background-color', 'background'],
+    answer: 'background-color',
+  },
+  {
+    question: "How do you select an element with the id 'header' in CSS?",
+    choices: ['*header', '#header', '.header', 'header'],
+    answer: '#header',
+  },
+  {
+    question:
+      'Which of the following is the correct syntax for making all the <p> elements bold?',
+    choices: [
+      'p {font-weight:bold;}',
+      '<p> {text-size:bold;}',
+      'p {text-style:bold;}',
+      '<p> style="font-size:bold;"',
+    ],
+    answer: 'p {font-weight:bold;}',
+  },
+  {
+    question: 'What is the defult value of the position property',
+    choices: ['fixed', 'absolute', 'relative', 'static'],
+    answer: 'static',
+  }
 ];
