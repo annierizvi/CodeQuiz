a57cdcdff709b5c2bb0754570d6465e94c31c3f5
Annie <anam.k@live.com>
2024-01-12 19:11:30 +0000

    added html question for the code quiz

---
+++ starter/assets/questions.js
@@ -0,0 +1,34 @@
+const questions = [
+  // HTML Questions
+  {
+    question: 'What does HTML stand for?',
+    answers: [
+      'Hyper Trainer Marking Language',
+      'Hyper Text Markup Language',
+      'Hyperlinks and Text Markup Language',
+      'Home Tool Markup Language',
+    ],
+    correct: 1,
+  },
+  {
+    question:
+      'Which of the following HTML elements is used for creating an unordered list?',
+    answers: ['<ui>', '<i>', '<ul>', '<list>'],
+    correct: 2,
+  },
+  {
+    question: 'Which HTML element is used to define a paragraph?',
+    answers: ['<pg>', '<paragraph>', '<p>', '<text>'],
+    correct: 2,
+  },
+  {
+    question: 'What is the purpose of the <div> tag in HTML?',
+    answers: [
+      'For creating titles',
+      'For creating a division or a section',
+      'For adding a line break',
+      'For emphasizing text',
+    ],
+    correct: 1,
+  },
+];
